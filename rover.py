76afa03bc27fc0d94f97922c72c5f1aeba076807
flemerrer <francois.lemerrer@protonmail.com>
2024-09-26 23:55:06 +0200

    write implementations for rover kata

--- rover.py
+++ rover.py
@@ -9,27 +9,72 @@ class Direction(Enum):
 
 
 class Rover:
-    def __init__(self):
-        self.direction = Direction.NORTH
-        self.x = 0
-        self.y = 0
+
+    def __init__(self, direction1 = Direction.NORTH, x = 0, y = 0):
+        self.direction = direction1
+        self.x = x
+        self.y = y
+
+    def go_forward(self):
+
+        match self.direction:
+            case Direction.NORTH:
+                self.y += 1
+            case Direction.WEST:
+                self.x -= 1
+            case Direction.SOUTH:
+                self.y -= 1
+            case Direction.EAST:
+                self.x += 1
+
+    def go_backward(self):
+
+        match self.direction:
+            case Direction.NORTH:
+                self.y -= 1
+            case Direction.WEST:
+                self.x += 1
+            case Direction.SOUTH:
+                self.y += 1
+            case Direction.EAST:
+                self.x -= 1
+
+    def rotate_left(self):
+
+        match self.direction:
+            case Direction.NORTH:
+                self.direction = Direction.WEST
+            case Direction.WEST:
+               self.direction = Direction.SOUTH
+            case Direction.SOUTH:
+                self.direction = Direction.EAST
+            case Direction.EAST:
+                self.direction = Direction.NORTH
+
+    def rotate_right(self):
+
+        match self.direction:
+            case Direction.NORTH:
+                self.direction = Direction.EAST
+            case Direction.EAST:
+               self.direction = Direction.SOUTH
+            case Direction.SOUTH:
+                self.direction = Direction.WEST
+            case Direction.WEST:
+                self.direction = Direction.NORTH
 
 
-def go_forward():
-    pass
+    def command_rover(self, commands):
 
-
-def go_backward():
-    pass
-
-
-def rotate_left():
-    pass
-
-
-def rotate_right():
-    pass
-
-
-def command_rover():
-    pass
+        for char in commands:
+            match char:
+                case 'F':
+                    self.go_forward()
+                case 'B':
+                    self.go_backward()
+                case 'R':
+                    self.rotate_right()
+                case 'L':
+                    self.rotate_left()
+                case _ :
+                    print('commande ' + str(char) + 'non reconnue.')
--- test_rover.py
+++ test_rover.py
@@ -2,22 +2,86 @@ from sqlite3.dbapi2 import paramstyle
 
 import pytest
 
+from rover import Rover, Direction
+
 
 def test_go_forward_should_increase_y_by_1_when_facing_north():
-    pass
+    # given
+    rover = Rover()
+    # when
+    rover.go_forward()
+    # then
+    assert rover.direction == Direction.NORTH
+    assert rover.y == 1
 
 
 def test_go_backward_should_decrease_y_by_1_when_facing_north():
-    pass
+    rover = Rover()
 
-@pytest.mark.parametrize('a, b, expected', [('Data', 1),('Data', 1)])
-def test_rotate_left_should_change_direction_but_keep_position_unchanged():
-    pass
+    rover.go_backward()
 
-
-def test_rotate_right_should_change_direction_but_keep_position_unchanged():
-    pass
+    assert rover.direction == Direction.NORTH
+    assert rover.y == -1
 
 
-def test_command_rover_should_move_rover_to_specified_coordinates():
-    pass
+def test_go_backward_should_decrease_x_by_1_when_facing_east():
+    rover = Rover(Direction.EAST)
+
+    rover.go_backward()
+
+    assert rover.direction == Direction.EAST
+    assert rover.x == -1
+
+
+@pytest.mark.parametrize('given_direction, expected_direction', [
+    (Direction.NORTH, Direction.WEST),
+    (Direction.WEST, Direction.SOUTH),
+    (Direction.SOUTH, Direction.EAST),
+    (Direction.EAST, Direction.NORTH)])
+def test_rotate_left_should_change_direction_but_keep_position_unchanged(given_direction, expected_direction):
+    rover = Rover(given_direction)
+
+    rover.rotate_left()
+
+    assert rover.direction == expected_direction
+    assert rover.y == 0 and rover.x == 0
+
+
+@pytest.mark.parametrize('given_direction, expected_direction', [
+    (Direction.NORTH, Direction.EAST),
+    (Direction.EAST, Direction.SOUTH),
+    (Direction.SOUTH, Direction.WEST),
+    (Direction.WEST, Direction.NORTH)])
+def test_rotate_right_should_change_direction_but_keep_position_unchanged(given_direction, expected_direction):
+    rover = Rover(given_direction)
+
+    rover.rotate_right()
+
+    assert rover.direction == expected_direction
+    assert rover.y == 0 and rover.x == 0
+
+
+@pytest.mark.parametrize('commands, expected_direction, expected_x, expected_y', [
+    ('FFLF', Direction.WEST, -1, 2),
+    ('BRFR', Direction.SOUTH, 1, -1),
+    ('LFFF', Direction.WEST, -3, 0)])
+def test_command_rover_should_move_rover_to_specified_coordinates(commands, expected_direction, expected_x, expected_y):
+    rover = Rover(Direction.NORTH, 0, 0)
+
+    rover.command_rover(commands)
+
+    assert rover.x == expected_x and rover.y == expected_y
+    assert rover.direction == expected_direction
+
+
+@pytest.mark.parametrize('commands, expected_direction, expected_x, expected_y', [
+    ('FFTF', Direction.NORTH, 0, 3),
+    ('A1FR', Direction.EAST, 0, 1),
+    ('LF-3', Direction.WEST, -1, 0)])
+def test_command_rover_should_ignore_incorrect_instructions(commands, expected_direction, expected_x, expected_y):
+    rover = Rover(Direction.NORTH, 0, 0)
+
+    rover.command_rover(commands)
+
+    assert rover.x == expected_x and rover.y == expected_y
+    assert rover.direction == expected_direction
